731e9c52bd1964dc9621cac9e3b953cdaa998a5e
melonunderground <middletondane@gmail.com>
2017-08-14 14:49:53 -0500

    Apply method to first 6 examples ending @line 24

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,22 +4,23 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1,"o")
+"zom".insert(2,"o")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".insert(-1," suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".insert(0,"the usual")
 # => "the usual suspects"
 
 # "The case of the disappearing last letter".<???>
@@ -32,7 +33,7 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
