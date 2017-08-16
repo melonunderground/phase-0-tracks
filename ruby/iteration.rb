4a8cca8d0607593705621d6239bc6c4f0cc592a5
melonunderground <middletondane@gmail.com>
2017-08-16 18:52:25 -0500

    Complete release 1 using .each and .map! updating iteration file with examples for sporting equipment and athletes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,4 @@
+=begin
 def insulter
   puts "hey you, what are you looking at!?"
   yield("boring","lazy")
@@ -5,3 +6,42 @@ def insulter
 end
 
 insulter { |insult1, insult2| puts "no offense but you are #{insult1} and #{insult2}" }
+
+#.each call
+sporting_equipment = ["homeplate","puck","hoop"]
+
+puts "Original data:"
+p sporting_equipment
+
+sporting_equipment.each do |equipment|
+  puts equipment
+end
+
+puts "After .each call:"
+p sporting_equipment
+
+#.map! call
+sporting_equipment = ["homeplate","puck","hoop"]
+
+puts "Original data:"
+p sporting_equipment
+
+sporting_equipment.map! do |equipment|
+  puts equipment
+  equipment.upcase
+end
+
+puts "After .map! call:"
+p sporting_equipment
+
+=end
+
+athletes = {
+  "basketball"=> "jordan",
+  "baseball"=> "ruth",
+  "football"=> "payton"
+}
+
+athletes.each do |sport, player|
+  puts "#{player} is really skillful at #{sport}, right?"
+end
