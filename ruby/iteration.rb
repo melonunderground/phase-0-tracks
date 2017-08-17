f387c5182a574df5ef35d32f78d84f856085cfe4
melonunderground <middletondane@gmail.com>
2017-08-16 19:53:00 -0500

    Complete all 4 release 2 array examples on interation.rb file

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -34,7 +34,7 @@ end
 puts "After .map! call:"
 p sporting_equipment
 
-=end
+
 
 athletes = {
   "basketball"=> "jordan",
@@ -45,3 +45,62 @@ athletes = {
 athletes.each do |sport, player|
   puts "#{player} is really skillful at #{sport}, right?"
 end
+
+
+letters = ["a","b","c","d","e","f","g","h"]
+#.delete_if call
+puts "Original data:"
+puts letters
+
+letters.delete_if {|letter| letter < "d"}
+
+puts "After .delete_if call"
+puts letters
+
+letters = ["a","b","c","d","e","f","g","h"]
+
+#.keep_if call
+puts "Original data:"
+puts letters
+
+letters.keep_if { |letter| letter < "d"}
+
+puts "After .keep_if call"
+puts letters
+
+
+letters = ["a","b","c","d","e","f","g","h"]
+
+#.select call
+puts "Original data:"
+puts letters
+
+modified_letters = letters.select { |letter| letter > "b"}
+
+puts "After .select() call"
+p modified_letters
+
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+#.take_while call
+
+puts "Original data:"
+puts numbers
+
+modified_numbers = numbers.take_while { |number| number < 3 }
+
+puts "After .take_while call"
+puts modified_numbers
+=end
+
+
+numbers = {1, 2, 3, 4, 5, 6, 7, 8}
+
+
+
+
+numbers = {1, 2, 3, 4, 5, 6, 7, 8}
+numbers = {1, 2, 3, 4, 5, 6, 7, 8}
+numbers = {1, 2, 3, 4, 5, 6, 7, 8}
