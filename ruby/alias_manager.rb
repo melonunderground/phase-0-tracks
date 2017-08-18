6d5b9bfc21fd53db0993d939d1da1166e29b02f6
melonunderground <middletondane@gmail.com>
2017-08-17 23:42:34 -0500

    Pseudocode spy name changer in alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+=begin
+#Pseudocode
+get a spy's real first and last name. swap the first and last name so the first name is last and last name is first
+seperate the letters of first and last name.
+if a letter is a vowel convert it to the next vowel in the order aeiou
+if a letter is a consonant convert it to the next consonant in the order bcdfgjklmnpqrstvwxyz
+create an interface that gets the spy's name and returns the new name with the tasks above processed on the name.
+continue returning new names until the spy types quit.
+=end
