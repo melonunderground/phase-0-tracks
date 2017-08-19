0b01ffc4b7b6eb812209c27577cfd73e9275fcb0
melonunderground <middletondane@gmail.com>
2017-08-18 22:24:59 -0500

    Add hash_test.rb to master

---
+++ ruby/hash_test.rb
@@ -0,0 +1,13 @@
+answers_interior_design = {
+  :name => "Steve",
+  :address => "123 Street",
+  :email => "you@email.com",
+  :phone => "847-123-4321",
+  :fave_shade_of_blue => "sky",
+  :wallpaper_preferences=> "Paisley,Chevrons",
+  :ombre_is=> "Fierce"
+}
+
+puts answers_interior_design[:name] + answers_interior_design[:address]
+answers_interior_design[:hired] = "yes"
+p answers_interior_design
