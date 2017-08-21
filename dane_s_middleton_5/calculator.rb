2ac5432eb90f48b9e4258b3561e76b325345a709
melonunderground <middletondane@gmail.com>
2017-08-20 21:06:27 -0500

    Answer remaining questions

--- dane_s_middleton_5/calculator.rb
+++ dane_s_middleton_5/calculator.rb
@@ -9,3 +9,18 @@ p calculator(4, "-", 5)
 p calculator(4, '/', 5)
 
 p calculator(4, '*', 5)
+puts "welcome to the calculator"
+loop do
+puts "please enter the first number you want to use in your calculation"
+int_1 = gets.chomp.to_i
+puts "please enter the operation you would like to calculate (+, -, *, or /)"
+operation = gets.chomp.to_s
+puts "please enter the second number you want to use in your calculation"
+int_2 = gets.chomp.to_i
+p calculator(int_1,operation,int_2)
+puts "would you like to enter another number and operation combo? to exit type 'done'"
+reply = gets.chomp
+  if reply == "done"
+  break
+  end
+end
