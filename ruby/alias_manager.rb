490ddb624f7e37dc2a474695da92195a265a7e16
melonunderground <middletondane@gmail.com>
2017-08-21 03:49:38 -0500

    Complete alias_manager name switch, next vowel, consonant adv methods.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,24 +1,24 @@
-=begin
-#Pseudocode
+
+
 get a spy's real first and last name. swap the first and last name so the first name is last and last name is first
 seperate the letters of first and last name.
 if a letter is a vowel convert it to the next vowel in the order aeiou
 if a letter is a consonant convert it to the next consonant in the order bcdfghjklmnpqrstvwxz
 create an interface that gets the spy's name and returns the new name with the tasks above processed on the name.
 continue returning new names until the spy types quit.
-=end
+
 
 def name_switch(name)
-new_name = name.split (' ')
-new_name = new_name.insert(1, " ")
-new_name.reverse!
-new_name_str = new_name.join('')
-new_name = new_name_str.split("")
+  new_name = name.split (' ')
+  new_name.reverse!
+  new_name_str = new_name.join(' ')
 end
-puts name_switch("dane middleton")
-=begin
+
+p name_switch("dane middleton")
+
+
 def next_vowel(name)
-  vowels = ["a", "e", "i", "o", "u",]
+  vowels = ["a", "e", "i", "o", "u"]
   name = name.split('')
   name.map! do |letter|
     if vowels.include?(letter)
@@ -27,13 +27,14 @@ def next_vowel(name)
       letter
     end
   end
-  puts name
+  puts name.join.downcase
 end
-next_vowel("dane")
 
-=begin
+p next_vowel("dane middleton")
+
+
 def consonant_adv(name)
-  consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y",z"]
+  consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
   name = name.split('')
   name.map! do |letter|
     if consonants.include?(letter)
@@ -42,8 +43,10 @@ def consonant_adv(name)
       letter
     end
   end
-  puts name
+  puts name.join.downcase
 end
-  consonant_adv("dazy")
-=end
+
+consonant_adv("dane middleton")
+
+
 
