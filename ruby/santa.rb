a5fed570d8fe421dee68059b50a129641663f7bb
melonunderground <middletondane@gmail.com>
2017-08-23 15:32:40 -0500

    Complete release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,14 @@
 class Santa
 
-  def initialize
+  def initialize (name, gender, ethnicity, hometown)
     puts "Initializing Santa instance..."
-  end
+    @name = name
+    @gender = gender
+    @ethnicity = ethnicity
+    @hometown = hometown
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+    end
 
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
@@ -12,11 +18,29 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
+  def welcome
+  puts "my name is #{@name} and i traveled here from #{@hometown}"
+  end
+
 end
 
-santa = Santa.new
-p santa.speak
-p santa.eat_milk_and_cookies("oatmeal")
+santas = []
+name = ["jeff","angela","mike","rick","sarah","belinda","maggie"]
+gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+hometown = ["ogdenville","haberbrook","shelbyville","detroit", "chicago","richland","brookville"]
+ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+gender.length.times do |i|
+  santas << Santa.new(name[i],gender[i], ethnicity[i], hometown[i])
+end
+santas.each do |santa|
+santa.welcome
+end
+
+
+
+
+
+
 
 
 
