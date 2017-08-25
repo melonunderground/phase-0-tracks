bc4eca8d9214f2193f7da3bf8b242ca7edeea9c9
melonunderground <middletondane@gmail.com>
2017-08-25 00:32:25 -0500

    Create shout module for grandparent and child classes and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,8 +1,10 @@
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
 end
+
 
 module Yelling_Happily
   def self.yell_happily(words)
@@ -12,3 +14,24 @@ end
 
 p Shout.yell_angrily("hoooray")
 p Yelling_Happily.yell_happily("argh")
+=end
+
+module Shout
+  def spaz_out(getting_mad)
+    puts "I can't take this anymore #{getting_mad}!"
+  end
+end
+
+class Child
+  include Shout
+end
+
+class Grandparent
+  include Shout
+end
+
+child = Child.new
+child.spaz_out("yaaaaaaaahhhhhh")
+
+plane = Grandparent.new
+plane.spaz_out("bllluuuurghhh")
