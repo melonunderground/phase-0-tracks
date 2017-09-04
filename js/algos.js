a55e07e6a4df92581ff8c79f52c54823567ccbb7
melonunderground <middletondane@gmail.com>
2017-09-04 11:31:43 -0500

    Complete release 0

---
+++ js/algos.js
@@ -0,0 +1,15 @@
+//pseudocode
+//cycle through each word in the group of words determining their length. store the first word //with any letters by comparing it to a letterless word. get the next word and compare it to the //currently stored word replacing it if the next word is longer. change the word length back to the word and display.
+
+
+var array = ["long phrase","longest phrase","longer phrase"];
+var letters = 0;
+var longestWord = " ";
+
+for (var i = 0; i < array.length; i++) {
+    if (array[i].length > letters) {
+    letters = array[i].length;
+    longestWord = array[i];
+    }
+}
+console.log(longestWord);
